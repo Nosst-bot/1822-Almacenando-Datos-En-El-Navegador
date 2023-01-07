b501770775d90934e8484b10865c835ef8368240
Kevin <kevinalex531@gmail.com>
2023-01-07 19:27:05 -0300

    clase 05 completada

---
+++ components/readTasks.js
@@ -0,0 +1,19 @@
+import { createTask } from './addTask.js';
+import dateElement from './dateElement.js';
+import { uniqueDates, orderDates } from '../services/date.js';
+
+export const displayTasks = () => {
+  const list = document.querySelector('[data-list]');
+  const tasksList = JSON.parse(localStorage.getItem('tasks')) || [];
+  const dates = uniqueDates(tasksList);
+  orderDates(dates)
+  dates.forEach((date) => {
+    const dateMoment = moment(date, 'DD/MM/YY');
+    list.appendChild(dateElement(date));
+    tasksList.forEach((task) => {
+      const taskDate = moment(task.dateFormat, 'DD/MM/YY');
+      const diff = dateMoment.diff(taskDate);
+      if( diff === 0) list.appendChild(createTask(task));
+    });
+  });
+};
---
+++ services/date.js
@@ -0,0 +1,15 @@
+export const uniqueDates = (tasks) => {
+  const unique = [];
+  tasks.forEach((task) => {
+    if (!unique.includes(task.dateFormat)) unique.push(task.dateFormat);
+  });
+  return unique;
+};
+
+export const orderDates = (dates) => {
+  return dates.sort((a, b) => {
+    const firstDate = moment(a, 'DD/MM/YY');
+    const secondDate = moment(b, 'DD/MM/YY');
+    return firstDate - secondDate;
+  });
+};
